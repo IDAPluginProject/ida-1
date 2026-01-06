38fae7a533aed7b45570ef01fa9ab2eb287880a3
NAKATSURU You <you@naomtaster.com>
2026-01-06 14:11:53 +0900

    Update findguid.py for IDA 9.2 support

--- idapython_tools/findguid/findguid.py
+++ idapython_tools/findguid/findguid.py
@@ -1,40 +1,5 @@
-import idc, idaapi, ida_search, ida_name, ida_bytes, ida_pro
+import idc, ida_idaapi, ida_name, ida_bytes, ida_typeinf
 import os, binascii, struct
-
-try:
-    import ida_struct
-    add_struc = ida_struct.add_struc
-    get_struc_id = ida_struct.get_struc_id
-    add_struc_member = ida_struct.add_struc_member
-    get_struc_size = ida_struct.get_struc_size
-    get_struc = ida_struct.get_struc
-    get_member_by_name = ida_struct.get_member_by_name
-except ModuleNotFoundError:
-    # from IDA 9.0, many of them are in idc.
-    add_struc = idc.add_struc
-    get_struc_id = idc.get_struc_id
-    add_struc_member = idc.add_struc_member
-    get_struc_size = idc.get_struc_size
-    
-    # for IDA 9.0
-    # Some of them are needed to implement.
-    def get_struc(struct_tid):
-        tif = ida_typeinf.tinfo_t()
-        if tif.get_type_by_tid(struct_tid):
-            if tif.is_struct():
-                return tif
-        return ida_idaapi.BADADDR
-    
-    def get_member_by_name(tif, name):
-        if not tif.is_struct():
-            return None
-    
-        udm = ida_typeinf.udm_t()
-        udm.name = name
-        idx = tif.find_udm(udm, ida_typeinf.STRMEM_NAME)
-        if idx != -1:
-            return udm
-        return None
 
 GUID_LIST_DIR = os.path.join(os.path.dirname(__file__), 'guid_list')
 GUID_LIST= []
@@ -45,16 +10,22 @@ GUID_LIST.append(['Folder ID', 'FOLDERID_', os.path.join(GUID_LIST_DIR, 'folder.
 GUID_LIST.append(['Media Type', '', os.path.join(GUID_LIST_DIR, 'media.txt')])
 
 def get_guid_tid():
-    tid = get_struc_id('GUID')
-    if tid == idaapi.BADADDR:
-        print("[*] create GUID struct")
-        tid = add_struc(0xffffffff, 'GUID', 0)
-        sptr = get_struc(tid)
-        add_struc_member(sptr, 'Data1', 0x0, 0x20000000, None, 4)
-        add_struc_member(sptr, 'Data2', 0x4, 0x10000000, None, 2)
-        add_struc_member(sptr, 'Data3', 0x6, 0x10000000, None, 2)
-        add_struc_member(sptr, 'Data4', 0x8, 0x00000000, None, 8)
-    return tid
+    tif = ida_typeinf.tinfo_t()
+    if not tif.get_named_type(None, 'GUID') or not tif.get_size() == 16:
+        print('[*] create GUID struct')
+        tif = ida_typeinf.tinfo_t()
+        guid_struct_str = """struct GUID {
+            unsigned int Data1;
+            unsigned short Data2;
+            unsigned short Data3;
+            unsigned char Data4[8];
+        };"""
+        ida_typeinf.del_named_type(None, 'GUID', ida_typeinf.NTF_TYPE)
+        ida_typeinf.idc_parse_types(guid_struct_str, 0)
+        if not tif.get_named_type(None, 'GUID'):
+            print('[!] failed to create GUID struct')
+            return ida_idaapi.BADADDR
+    return tif.force_tid()
 
 def make_binary_pattern(guid):
     # sample guid: 0F87369F-A4E5-4CFC-BD3E-73E6154572DD
@@ -71,6 +42,10 @@ def make_binary_pattern(guid):
 
 def main():
     tid = get_guid_tid()
+    if tid == ida_idaapi.BADADDR:
+        print('[!] failed to tid for GUID struct')
+        return
+
     for type_name, type_prefix, filepath in GUID_LIST:
         print('[*] scanning {}'.format(type_name))
         fp = open(filepath, 'r')
@@ -84,28 +59,24 @@ def main():
 
             ea = 0
             while True:
-                if ida_pro.IDA_SDK_VERSION >= 900:
-                    ea = ida_bytes.find_bytes(binary_pattern, ea, flags=ida_bytes.BIN_SEARCH_FORWARD | ida_bytes.BIN_SEARCH_NOSHOW)
-                else:
-                    ea = idc.find_binary(ea, ida_search.SEARCH_DOWN | ida_search.SEARCH_NEXT | ida_search.SEARCH_NOSHOW, binary_pattern)
-                if ea == idaapi.BADADDR:
+                ea = ida_bytes.find_bytes(binary_pattern, ea, flags=ida_bytes.BIN_SEARCH_FORWARD | ida_bytes.BIN_SEARCH_NOSHOW)
+                if ea == ida_idaapi.BADADDR:
                     break
 
                 idc.del_items(ea, 16, 0)
-                ida_bytes.create_struct(ea, get_struc_size(tid), tid)
+                ida_bytes.create_struct(ea, 16, tid)
                 if idc.set_name(ea, guid_name, ida_name.SN_NOWARN) != 1:
                     for i in range(0, 100):
                         if idc.set_name(ea, guid_name + "_" + str(i), ida_name.SN_NOWARN) == 1:
                             break
                     else:
-                        print("[!] 0x{:X}: failed to apply {}".format(ea, guid_name))
-                print("[*] 0x{:X}: {}".format(ea, guid_name))
+                        print('[!] 0x{:X}: failed to apply {}'.format(ea, guid_name))
+                print('[*] 0x{:X}: {}'.format(ea, guid_name))
                 
                 # add a byte size for find_bytes because it does not have SEARCH_NEXT option like find_binary
-                if ida_pro.IDA_SDK_VERSION >= 900:
-                    ea += 1
+                ea += 1
 
-    print("[*] finished")
+    print('[*] finished')
 
 if __name__ == "__main__":
     main()
